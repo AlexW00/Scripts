fc6d312123a9705d75b0cc9881d7c7f5cb82ca4a
Alexander <alexanderweichart@fastmail.com>
2023-06-27 11:05:29 +0200

    container indicator

---
+++ Bash/container_indicator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if we're in a Docker container
+if [ -f /.dockerenv ]; then
+    echo "🐋 $hostname"
+    exit 0
+fi
+
+if [ ! -z "$CONTAINER_ID" ]; then
+    echo "📦️ $CONTAINER_ID"
+    exit 0
+fi
+
+if [ ! -z "${container}" ]; then
+    echo "📦️ ${container}"
+    exit 0
+fi
+
+echo ""
+exit 0
