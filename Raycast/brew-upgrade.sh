8199b8bb87d915941d133e6143cee338741d7ac4
Alexander Weichart <alexanderweichart@fastmail.com>
2025-04-23 18:44:48 +0200

    add 2 raycast scripts

---
+++ Raycast/brew-upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Brew - Upgrade
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⬇️
+# @raycast.packageName Brew
+
+# Documentation:
+# @raycast.author Alex Weichart
+
+brew upgrade --cask
